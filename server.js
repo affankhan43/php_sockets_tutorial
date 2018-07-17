0dfc72f81f9c22db6e4c84d08250c2998ba5ba94
affankhan43 <affankhan627@gmail.com>
2018-07-18 00:49:08 +0500

    sockets

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ logger.info('SocketIO > listening on port');
 
 var app = express();
 var cors = require('cors');
-var http_server = https.createServer(app.use(cors({origin:'*'}))).listen(3001,'0.0.0.0');
+var http_server = https.createServer({
+  cert: fs.readFileSync('/etc/letsencrypt/live/ws.pixiubit.com/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/ws.pixiubit.com/privkey.pem')
+  },app.use(cors({origin:'*'}))).listen(3001,'0.0.0.0');
     function emitNewOrder(http_server){
       var io = socket.listen(http_server);
       var request = require("request");
